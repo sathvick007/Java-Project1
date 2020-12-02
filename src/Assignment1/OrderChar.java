0e1e7131d19092ee85fcc8bf2ea42bd0cb92f611
sathvick <sathvick.texas@gmail.com>
2020-12-02 17:38:15 -0600

    OrderChar
    Initialize two character variables in a program and display the characters in alphabetical order.

--- src/Assignment1/OrderChar.java
+++ src/Assignment1/OrderChar.java
@@ -1,4 +1,14 @@
 package Assignment1;
 
 public class OrderChar {
+    public static void main(String[] args)  {
+        char a = 'a';
+        char c  = 'z';
+
+        if(a<c)  {
+            System.out.println(a+","+c);
+        }
+        else
+            System.out.println(c+  ","  + a);
+    }
 }
